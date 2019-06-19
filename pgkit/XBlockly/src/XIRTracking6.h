00748dee10699f806139ee7b8d517d22f629ec7f
i7thtool <i7thtool@qq.com>
2019-06-19 17:02:35 +0800

    fix pgkit

---
+++ pgkit/XBlockly/src/XIRTracking6.h
@@ -0,0 +1,39 @@
+/* XIRTracking6.h
+ *
+ * Copyright (C) 2017-2022 Shanghai Mylecon Electronic Technology Co., Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program;
+ *
+ * Description: 
+ *     This file is a header file for XIRTracking6.
+ *
+ * Version: 1.0.0
+ */
+ 
+
+#ifndef __XIRTRACKING6_H__
+#define __XIRTRACKING6_H__
+
+
+#include <XIRTracking.h>
+
+
+class XIRTracking6 : public XIRTracking  {
+public:
+	XIRTracking6() {}
+	~XIRTracking6() {}
+
+};
+
+#endif //__XIRTRACKING6_H__
